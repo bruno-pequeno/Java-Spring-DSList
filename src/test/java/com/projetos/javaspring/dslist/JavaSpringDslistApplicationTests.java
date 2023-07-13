7f207463a22cf205add5f07cf7cd434cb3709dbf
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-13 13:08:20 -0300

    Iniciando o projeto

---
+++ src/test/java/com/projetos/javaspring/dslist/JavaSpringDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetos.javaspring.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
