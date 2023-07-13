399aba08f24e8e4e0513f67672122eae7ce394fa
Bruno Pequeno <bru.pequeno@gmail.com>
2023-07-13 13:15:50 -0300

    Get Lists

---
+++ src/main/java/com/projetos/javaspring/dslist/dto/GameMinDTO.java
@@ -0,0 +1,52 @@
+package com.projetos.javaspring.dslist.dto;
+
+import com.projetos.javaspring.dslist.entities.Game;
+import com.projetos.javaspring.dslist.projections.GameMinProjection;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class GameMinDTO {
+
+    private long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameMinDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+    public GameMinDTO(GameMinProjection projection){
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+}
